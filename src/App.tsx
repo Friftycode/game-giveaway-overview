3b3b1b87e2623e2adb86cd4290f0b19adcc57917
Friftycode <arthur.leonard.thomassen@gmail.com>
2025-06-14 23:41:02 +0200

    chore: Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import HomePage from "./page/HomePage";
+import "./App.less";
+
+function App() {
+  return <HomePage />;
+}
+
+export default App;
---
+++ src/page/HomePage.tsx
@@ -0,0 +1,12 @@
+import styles from "./HomePage.module.less";
+
+const HomePage = () => {
+  return (
+    <div className={styles.homePage}>
+      <h2>Welcome to the Home Page</h2>
+      <p>Start your project.</p>
+    </div>
+  );
+};
+
+export default HomePage;
